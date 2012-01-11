f8455db2b9c030a08b3e00f798ce6b9894786c97
Armando Fox <fox@cs.berkeley.edu>
2012-01-11 15:23:19 -0800

    Now has add-file on front page and delete buttons for files

--- bookware.py
+++ bookware.py
@@ -1,53 +1,48 @@
 #!/usr/bin/env python
 #
 
-import os
 import urllib
+import logging
 
 from google.appengine.ext import blobstore
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import blobstore_handlers
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
-
-class AddFileHandler(webapp.RequestHandler):
-    def get(self):
-        upload_url = blobstore.create_upload_url('/admin/upload')
-        body = """
-              <form action="%s" method="POST" enctype="multipart/form-data">
-                Upload File:  
-                <input type="file" name="file"><br/> 
-                <input type="submit" name="submit" value="Upload"> 
-              </form>
-        """ % upload_url
-        self.response.out.write(template.render('template.html', {'body': body}))
+from google.appengine.api.users import is_current_user_admin
 
 class UploadHandler(blobstore_handlers.BlobstoreUploadHandler):
     def post(self):
         upload_files = self.get_uploads('file')  # 'file' is file upload field in the form
         blob_info = upload_files[0]
-        body = """
-          <h3>Uploaded file '%s' (%d bytes) as /files/%s</h3>
-          <p>
-            <a href="/admin/add">Add another</a> | <a href="/">List</a>
-          </p>
-        """  % (blob_info.filename, blob_info.size, blob_info.key())
-        self.response.out.write(template.render('template.html', {'body': body}))
+        self.redirect('/')
+
+class DeleteHandler(webapp.RequestHandler):
+    def post(self, urlpath):
+        blobname = urllib.unquote(urlpath)
+        logging.debug("Deleting " + blobname)
+        blobstore.delete(blobname)
+        self.redirect('/')
 
 class ListHandler(webapp.RequestHandler):
     def get(self):
-        self.response.out.write(template.render('index.html', {'blobs': blobstore.BlobInfo.all() }))
+        upload_url = blobstore.create_upload_url('/admin/upload')
+        params = {'admin': is_current_user_admin(), 
+                  'upload_url': upload_url, 
+                  'blobs': blobstore.BlobInfo.all() }
+        self.response.out.write(template.render('index.html', params))
 
 class ServeHandler(blobstore_handlers.BlobstoreDownloadHandler):
     def get(self, resource):
         resource = str(urllib.unquote(resource))
         blob_info = blobstore.BlobInfo.get(resource)
-        self.send_blob(blob_info)
+        self.send_blob(blob_info, save_as=True)
 
 def main():
+    logging.getLogger().setLevel(logging.DEBUG)
     application = webapp.WSGIApplication(
-          [('/admin/add', AddFileHandler),
-           ('/admin/upload', UploadHandler),
+          [('/admin/upload', UploadHandler),
+           ('/admin/delete/(.*)$', DeleteHandler),
            ('/files/?$', ListHandler),
            ('/?$', ListHandler),
            ('/files/([^/]+)?', ServeHandler),
