4a6c1f360db5c309bcc3cddab25a16e0fe9f4e41
Armando Fox <fox@cs.berkeley.edu>
2015-08-26 09:37:16 -0700

    removed obsolete hw2edxml, moved to its own repo as hw2olx

--- hw2edxml/spec/hw2edxml_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-require 'hw2edxml'
-describe 'CS169x homework to edXML converter' do
-
-end
--- hw2edxml/spec/spec_helper.rb
+++
@@ -1,4 +0,0 @@
-require 'its'
-require 'debugger'
-Dir[File.join(File.dirname(__FILE__), '..', 'lib', '*.rb')].each { |f|  load f }
-Dir[File.join(File.dirname(__FILE__), '/support', '**', '*.rb')].each {|f| load f}
