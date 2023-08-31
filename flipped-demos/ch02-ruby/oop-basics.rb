5bf8bf03ccc1c9b600b63ad304251e25725bc932
Armando Fox <fox@cs.berkeley.edu>
2023-08-31 00:52:35 -0700

    *** empty log message ***

---
+++ flipped-demos/ch02-ruby/oop-basics.rb
@@ -0,0 +1,21 @@
+class Account
+
+  def initialize(starting_balance)
+    @balance = starting_balance
+  end
+
+end
+
+# what happens if we don't provide an argument to the constructor?
+# could we change that to have it default to $0?
+
+# create a new instance of an account with a $1000 balance:
+
+
+# ask the account what its balance is:
+
+
+# try to set the balance:
+
+
+# withdraw or deposit:
