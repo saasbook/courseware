85dd9f98fe926d75c39146d1df01fb47255c62fe
Armando Fox <fox@cs.berkeley.edu>
2023-09-14 00:13:38 -0700

    playing with the sinatra app

--- flipped-demos/ch03-saas/sinatra-sessions/app.rb
+++ flipped-demos/ch03-saas/sinatra-sessions/app.rb
@@ -1,10 +1,18 @@
 require 'sinatra'
+require 'byebug'
 
 class DemoApp < Sinatra::Base
 
+  enable :sessions
+
   get '/hello/:something' do
     @content = params[:something]
+    session[:thing] = @content
     erb :hello
   end
-  
+
+  get '/remember' do
+    @remembered = session[:thing]
+    erb :remember
+  end
 end
