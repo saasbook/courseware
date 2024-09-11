665a223b9e9b2758544631f6fdb0f7f0aa25751e
Michael Ball <michael@mball.co>
2024-09-11 15:49:25 -0700

    minor style / notes

--- flipped-demos/ch02-ruby/currency.rb
+++ flipped-demos/ch02-ruby/currency.rb
@@ -1,7 +1,8 @@
+# All a Number (a numeric in ruby) to have a method `to_yen`, `to_euros`, etc.
 module CurrencyConversions
-  @@currencies = {'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019}
-  def method_missing(method_id, *args, &block)  # capture all args in case have to call super
-    singular_currency = method_id.to_s.gsub( /s$/, '')
+  @@currencies = {'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019, 'usd' => 1 }
+  def method_missing(method_id, *args, &block) # capture all args in case have to call super
+    singular_currency = method_id.to_s..gsub(/^to_/, '').gsub( /s$/, '')
     if @@currencies.has_key?(singular_currency)
       self * @@currencies[singular_currency]
     else
@@ -13,5 +14,3 @@ end
 class Numeric
   include CurrencyConversions
 end
-
-
