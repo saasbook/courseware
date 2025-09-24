7d5fea70172eef1ce3d4a5dbd79775c268ac720d
Michael Ball <michael@mball.co>
2025-09-24 13:32:15 -0700

    Minor lecture updates for FA25

--- flipped-demos/ch03-saas/sinatra-sessions/app.rb
+++ flipped-demos/ch03-saas/sinatra-sessions/app.rb
@@ -1,12 +1,17 @@
 require 'sinatra'
 class DemoApp < Sinatra::Base
+  enable :sessions
 
   get '/' do
-    "hello world!"
-    # erb :hello
+    # "hello cs169A!"
+    puts session
+    @someone = session[:thing]
+    # @data = ['CS169A', 'CS168', 'CS162']
+    erb(:hello)
   end
 
   get '/set/:something' do
+    puts params
     session[:thing] = params[:something]
     redirect '/'
   end
